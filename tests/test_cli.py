ccf4dc85fe4414be59159bbbfec5dea015e9df5b
huideyeren <takakura.yusuke@gmail.com>
2021-12-20 19:50:13 +0900

    add cli tests.

---
+++ tests/test_cli.py
@@ -0,0 +1,26 @@
+import fire
+import pytest
+from anshitsu.cli import process
+
+
+def test_main_for_dir(capsys):
+    fire.Fire(process, ["./tests/pic"])
+    captured = capsys.readouterr()
+    result = captured.out
+    assert "The process was completed successfully." in result
+
+
+def test_main_for_file(capsys):
+    fire.Fire(process, ["./tests/pic/dog.jpg"])
+    captured = capsys.readouterr()
+    result = captured.out
+    assert "The process was completed successfully." in result
+
+
+def test_main_error(capfd):
+    with pytest.raises(SystemExit):
+        fire.Fire(process, ["pic"])
+    captured = capfd.readouterr()
+    error = captured.err
+
+    assert "A non-path string was passed." in error
